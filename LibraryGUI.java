da928f2723be315670e716bc609057c887781dcf
nhshohid <nhshohid1024@gmail.com>
2025-11-11 00:30:00 -0500

    11/10 Finished
    worked on the home page filter panel. text, size, color, all set to change. still needs action implementers and stuff.

--- LibraryGUI.java
+++ LibraryGUI.java
@@ -1,11 +1,11 @@
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
-
 import java.awt.event.ActionListener;
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -17,8 +17,14 @@ import java.awt.event.ActionEvent;
 
 public class LibraryGUI extends JFrame implements ActionListener { 
 
-    JPanel homePageHeaderPanel, filterPanel;
-    JLabel libraryTitleLabel, libraryIconLabel;
+    JPanel homePagePanel, homePageHeaderPanel, filterPanel;
+
+    JLabel libraryTitleLabel, libraryIconLabel, filterSortLabel, availabilityLabel,
+    coverTypeLabel, genreLabel;
+
+    JCheckBox availabilityCheckBox, paperbackCheckBox, hardcoverCheckBox, horrorCheckBox,
+    mysteryCheckBox, fantasyCheckBox;
+
     JButton goToListButton;
     ImageIcon libraryIcon, goToListIcon;
 
@@ -27,7 +33,7 @@ public class LibraryGUI extends JFrame implements ActionListener {
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(1080,720);
-        this.setLayout(new BorderLayout(0,100));
+        this.setLayout(new BorderLayout());
 
         // -------------- HOME PAGE --------------- // 
         
@@ -50,21 +56,86 @@ public class LibraryGUI extends JFrame implements ActionListener {
         homePageHeaderPanel = new JPanel(new BorderLayout());
         homePageHeaderPanel.setBackground(Color.lightGray);
         homePageHeaderPanel.setPreferredSize(new Dimension(1080,150));
-        homePageHeaderPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK,3));
+        homePageHeaderPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
         homePageHeaderPanel.add(libraryIconLabel, BorderLayout.WEST);
         homePageHeaderPanel.add(libraryTitleLabel);
         homePageHeaderPanel.add(goToListButton, BorderLayout.EAST);
 
+        filterSortLabel = new JLabel("Sort By: ");
+        filterSortLabel.setFont(new Font("MV Boli", Font.PLAIN, 25));
+        filterSortLabel.setBounds(15, 15, 200, 30);
 
-        filterPanel = new JPanel();
+        availabilityLabel = new JLabel("Availability: ");
+        availabilityLabel.setFont(new Font("MV Boli", Font.PLAIN, 18));
+        availabilityLabel.setBounds(15,70,200,20);
+        
+        availabilityCheckBox = new JCheckBox("In Stock");
+        availabilityCheckBox.setFocusable(false);
+        availabilityCheckBox.setBackground(Color.lightGray);
+        availabilityCheckBox.setBounds(15, 95, 150, 30);
+
+        coverTypeLabel = new JLabel("Cover Type: ");
+        coverTypeLabel.setFont(new Font("MV Boli", Font.PLAIN, 18));
+        coverTypeLabel.setBounds(15,145,200,20);
+
+        paperbackCheckBox = new JCheckBox("Paperback");
+        paperbackCheckBox.setFocusable(false);
+        paperbackCheckBox.setBackground(Color.lightGray);
+        paperbackCheckBox.setBounds(15, 170, 150, 30); 
+
+        hardcoverCheckBox = new JCheckBox("Hardcover");
+        hardcoverCheckBox.setFocusable(false);
+        hardcoverCheckBox.setBackground(Color.lightGray);
+        hardcoverCheckBox.setBounds(15, 200, 150, 30); 
+
+        genreLabel = new JLabel("Genre: ");
+        genreLabel.setFont(new Font("MV Boli", Font.PLAIN, 20));
+        genreLabel.setBounds(15,250,200,20);
+
+        horrorCheckBox = new JCheckBox("Horror");
+        horrorCheckBox.setFocusable(false);
+        horrorCheckBox.setBackground(Color.lightGray);
+        horrorCheckBox.setBounds(15, 275, 150, 30); 
+
+        mysteryCheckBox = new JCheckBox("Mystery");
+        mysteryCheckBox.setFocusable(false);
+        mysteryCheckBox.setBackground(Color.lightGray);
+        mysteryCheckBox.setBounds(15, 305, 150, 30); 
+
+        fantasyCheckBox = new JCheckBox("Fantasy");
+        fantasyCheckBox.setFocusable(false);
+        fantasyCheckBox.setBackground(Color.lightGray);
+        fantasyCheckBox.setBounds(15, 335, 150, 30); 
+
+        
+        filterPanel = new JPanel(null);
         filterPanel.setBackground(Color.lightGray);
         filterPanel.setPreferredSize(new Dimension(200,1080));
+        filterPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK,1));
+        filterPanel.add(filterSortLabel);
+        filterPanel.add(availabilityLabel);
+        filterPanel.add(availabilityCheckBox);
+        filterPanel.add(coverTypeLabel);
+        filterPanel.add(paperbackCheckBox);
+        filterPanel.add(hardcoverCheckBox);
+        filterPanel.add(genreLabel);
+        filterPanel.add(horrorCheckBox);
+        filterPanel.add(mysteryCheckBox);
+        filterPanel.add(fantasyCheckBox);
 
 
-        this.add(filterPanel, BorderLayout.WEST);
-        this.add(homePageHeaderPanel, BorderLayout.NORTH);
 
- 
+
+
+        homePagePanel = new JPanel(new BorderLayout(0, 100));
+        homePagePanel.add(homePageHeaderPanel, BorderLayout.NORTH);
+        homePagePanel.add(filterPanel, BorderLayout.WEST);
+
+
+        
+
+
+        this.add(homePagePanel, BorderLayout.CENTER);
         this.setVisible(true);
     }
 
